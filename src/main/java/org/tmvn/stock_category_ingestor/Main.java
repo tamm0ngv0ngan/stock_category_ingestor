d10dc4c0464abff2c67626f1ca5ce2a0a464a055
ducnguyen <tamm0ngv0ngan95@gmail.com>
2026-02-13 13:07:01 +0700

    Add gradle for build jar and log

---
+++ src/main/java/org/tmvn/stock_category_ingestor/Main.java
@@ -0,0 +1,10 @@
+package org.tmvn.stock_category_ingestor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.error("Test");
+    }
+}
