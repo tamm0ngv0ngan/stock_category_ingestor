6db008ba555ffb757b615ebd5904c07fcb63334e
ducnguyen <tamm0ngv0ngan95@gmail.com>
2026-04-08 23:59:04 +0700

    Add Code for ImportOption

--- src/main/java/org/tmvn/stock_category_ingestor/model/ImportOption.java
+++ src/main/java/org/tmvn/stock_category_ingestor/model/ImportOption.java
@@ -1,11 +1,11 @@
 package org.tmvn.stock_category_ingestor.model;
 
-public record ImportOption(int id, String value) {
+public record ImportOption(int id, String code, String value) {
     public static ImportOption ofStockTransaction() {
-        return new ImportOption(1, "Stock Transaction");
+        return new ImportOption(1, "transaction.file", "Stock Transaction");
     }
 
     public static ImportOption ofExerciseType() {
-        return new ImportOption(2, "Exercise Type");
+        return new ImportOption(2, "exercise.file", "Exercise Type");
     }
 }
